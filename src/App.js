4044bef7e7442c1c244e7b95bb1f8249c7633b32
Adam gif <kycksto03@gmail.com>
2022-09-11 02:21:05 +0700

    first build

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { ARCanvas, ARMarker } from "@artcom/react-three-arjs";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ARCanvas
+    camera={{ position: [0, 0, 0] }}
+    dpr={window.devicePixelRatio}
+    onCreated={({ gl }) => {
+      gl.setSize(window.innerWidth, window.innerHeight);
+    }}
+  >
+    <ambientLight />
+    <pointLight position={[10, 10, 0]} />
+    <ARMarker type={"pattern"} patternUrl={"data/hiro.patt"}>
+      <mesh>
+        <boxBufferGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color={"green"} />
+      </mesh>
+    </ARMarker>
+  </ARCanvas>
   );
 }
 
